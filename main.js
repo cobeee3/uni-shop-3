3792a3d3bdd62df87fb6e629a644f6907ad1247f
nautilus <cobeee@163.com>
2022-11-16 18:01:25 +0800

    add Home

--- main.js
+++ main.js
@@ -2,9 +2,27 @@
 // #ifndef VUE3
 import Vue from 'vue'
 import App from './App'
+import { $http } from '@escook/request-miniprogram'
 
+uni.$http = $http;
+$http.baseUrl = 'https://www.uinav.com'
+uni.$showMsg = function(title="获取资源失败",duration=1500){
+  uni.showToast({
+    title,
+    duration,
+    icon:'none'
+  })
+}
+
+$http.beforeRequest = function(options){
+  uni.showLoading({
+    title:'数据加载中...'
+ });
+}
+$http.afterRequest = function(){
+ uni.hideLoading()
+}
 Vue.config.productionTip = false
-
 App.mpType = 'app'
 
 const app = new Vue({
